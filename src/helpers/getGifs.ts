97829c85d061ae55c4f78811fe683eff7588d18c
Luciana <luciana.holzmann@gmail.com>
2024-06-11 02:57:10 +0200

    [FEAT] GetGifs

---
+++ src/helpers/getGifs.ts
@@ -0,0 +1,18 @@
+
+export const getGifs = async ( category: string ) => {
+
+    const api_key = "TbRyEoBgtxWPs2sbrBnbHFT6lbD9xbOw";
+    const URL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=10`;
+
+    const respuesta = await fetch(URL);
+    const { data } = await respuesta.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+
+  };
