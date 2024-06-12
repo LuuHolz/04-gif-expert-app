5ea42897b25c85f5402ed11a4d89583b18f192d5
Luciana <luciana.holzmann@gmail.com>
2024-06-13 00:09:08 +0200

    [TESTING] Pages: GifExpertApp

--- tests/pages/GifExpertApp.test.tsx
+++ tests/pages/GifExpertApp.test.tsx
@@ -1,12 +1,40 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { GifExpertApp } from '../../src/pages/GifExpertApp';
 
 describe('Pruebas en GifExpertApp', ( ) => { 
 
-    test('should ', () => {
+    test('Debe hacer match con el snapshot', () => {
       
-        render( <GifExpertApp/> );
-        screen.debug();
+        const { container } = render( <GifExpertApp/> );
+        expect( container ).toMatchSnapshot();
+    });
 
-    })
+    test('Debe agregar una nueva categoria', () => {
+        render( <GifExpertApp/> );
+
+        const input = screen.getByRole('textbox') as HTMLInputElement;
+        const form = screen.getByRole('form');
+
+        //simular evento
+        fireEvent.input( input, {target: { value: 'NewCategory'}});
+        fireEvent.submit( form );
+
+        expect(screen.getByText('NewCategory')).toBeTruthy();
+    });
     
- })
+    test('No debe agregar nueva categoria si ya existe', () => {
+        const category = "Meta";
+
+        render(<GifExpertApp/>);
+    
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+    
+        fireEvent.input(input, {target:{value: category}});
+        fireEvent.submit(form);
+
+        expect(screen.getAllByText(category).length).toBe(1);
+    });
+    
+    
+ });
