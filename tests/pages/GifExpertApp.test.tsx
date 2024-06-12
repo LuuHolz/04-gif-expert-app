d96768805fd2a5a893908e8ee4a2c9638424fddb
Luciana <luciana.holzmann@gmail.com>
2024-06-12 23:07:37 +0200

    [TESTING] CustomHook: useFetchGifs

---
+++ tests/pages/GifExpertApp.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+describe('Pruebas en GifExpertApp', ( ) => { 
+
+    test('should ', () => {
+      
+        render( <GifExpertApp/> );
+        screen.debug();
+
+    })
+    
+ })
